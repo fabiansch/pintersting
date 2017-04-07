14f5295ce23e093de442d711e59cf65b172b5439
fabian <stahlschiff@web.de>
2017-04-07 08:42:59 +0200

    update gems

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20131216081242) do
 
-  create_table "pins", force: true do |t|
+  create_table "pins", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -22,11 +21,10 @@ ActiveRecord::Schema.define(version: 20131216081242) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
-  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
-
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -44,9 +42,8 @@ ActiveRecord::Schema.define(version: 20131216081242) do
     t.string   "location"
     t.boolean  "mood"
     t.text     "aboutme"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
